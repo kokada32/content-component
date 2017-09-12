d4b887df593c57a2e5bcf034f1e971345a24a18b
Kenny Okada <kokada32@gmail.com>
2017-09-12 15:25:59 -0700

    adding content component

---
+++ ContentItemComponentService.cs
@@ -0,0 +1,194 @@
+﻿using Sabio.Data.Providers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sabio.Models.Domain;
+using Sabio.Data;
+using Sabio.Models.Requests;
+
+namespace Sabio.Services
+{
+    public class ContentItemComponentService : IContentItemComponentService
+    {
+        readonly IDataProvider dataProvider;
+        readonly Logger logger;
+        public ContentItemComponentService(IDataProvider dataProvider, Logger logger)
+        {
+            this.dataProvider = dataProvider;
+            this.logger = logger;
+        }
+
+        public ContentItemComponent GetById(int id)
+        {
+            var timer = logger.StartTimer("dbo.ContentItems_SelectByContentId, dbo.Subjects_SelectByContentId");
+            ContentItemComponent singleItem = null;
+
+            dataProvider.ExecuteCmd("dbo.ContentItems_SelectByContentId"
+               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+               {
+                   paramCollection.AddWithValue("@Id", id);
+               }
+               , singleRecordMapper: delegate (IDataReader reader, short set)
+               {
+                   singleItem = new ContentItemComponent();
+                   int startingIndex = 0;
+
+                   singleItem.Id = reader.GetSafeInt32(startingIndex++);
+                   singleItem.Contents = reader.GetSafeString(startingIndex++);
+                   singleItem.ContentTitle = reader.GetSafeString(startingIndex++);
+                   singleItem.ContentType = reader.GetSafeString(startingIndex++);
+                   singleItem.UserId = reader.GetSafeInt32(startingIndex++);
+                   singleItem.DateCreated = reader.GetSafeUtcDateTime(startingIndex++);
+
+               }
+               );
+
+            dataProvider.ExecuteCmd("dbo.Subjects_SelectByContentId"
+               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+               {
+                   paramCollection.AddWithValue("@Id", id);
+               }
+               , singleRecordMapper: delegate (IDataReader reader, short set)
+               {
+                   Subject listItems = new Subject();
+                   int startingIndex = 0;
+
+                   listItems.Id = reader.GetSafeInt32(startingIndex++);
+                   listItems.Category = reader.GetSafeString(startingIndex++);
+
+                   if (singleItem.Subjects == null)
+                   {
+                       singleItem.Subjects = new List<Subject>();
+                   }
+
+                   singleItem.Subjects.Add(listItems);
+               });
+            timer.Finish();
+            return singleItem;
+
+
+        }
+
+
+        public List<Subject> GetAllCategories()
+        {
+            List<Subject> list = null;
+
+            dataProvider.ExecuteCmd("dbo.Subjects_SelectAll"
+                , inputParamMapper: null
+                , singleRecordMapper: delegate (IDataReader reader, short set)
+                  {
+                      Subject singleItem = new Subject();
+                      int startingIndex = 0;
+
+                      singleItem.Id = reader.GetSafeInt32(startingIndex++);
+                      singleItem.Category = reader.GetSafeString(startingIndex++);
+
+                      if (list == null)
+                      {
+                          list = new List<Subject>();
+                      }
+
+                      list.Add(singleItem);
+                  }
+                );
+            return list;
+        }
+        
+
+        public List<ContentItem> GetRecentContents()
+        {
+            List<ContentItem> list = null;
+
+            dataProvider.ExecuteCmd("dbo.ContentItems_SelectRecentContents"
+                , inputParamMapper: null
+                , singleRecordMapper: delegate (IDataReader reader, short set)
+                {
+                    ContentItem singleContent = new ContentItem();
+                    int startingIndex = 0; //starting ordinal
+
+                    singleContent.Id = reader.GetSafeInt32(startingIndex++);
+                    singleContent.ContentTitle = reader.GetSafeString(startingIndex++);
+                    singleContent.UserId = reader.GetSafeInt32(startingIndex++);
+
+                    if (list == null)
+                   {
+                       list = new List<ContentItem>();
+                   }
+
+                   list.Add(singleContent);
+
+                });
+
+            return list;
+        }
+
+        public List<ContentItem> GetContentsBySubjectId(int subjectId, getContentRequest model)
+        {
+            List<ContentItem> list = null;
+
+            dataProvider.ExecuteCmd("dbo.ContentSubject_SelectBySubjectId"
+               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+               {
+                   paramCollection.AddWithValue("@SubjectId", subjectId);
+                   paramCollection.AddWithValue("@ItemNum", model.ItemNum);
+                   paramCollection.AddWithValue("@PageNum", model.PageNum);
+               }
+               , singleRecordMapper: delegate (IDataReader reader, short set)
+               {
+                   ContentItem singleContent = new ContentItem();
+                   int startingIndex = 0; //starting ordinal
+
+                   singleContent.Id = reader.GetSafeInt32(startingIndex++);
+                   singleContent.ContentTitle = reader.GetSafeString(startingIndex++);
+                   singleContent.UserId = reader.GetSafeInt32(startingIndex++);
+
+                   if (list == null)
+                   {
+                       list = new List<ContentItem>();
+                   }
+
+                   list.Add(singleContent);
+
+               });
+
+            return list;
+        }
+
+
+        public List<ContentItem> GetAllContents(getContentRequest model)
+        {
+            List<ContentItem> list = null;
+
+            dataProvider.ExecuteCmd("dbo.ContentItems_SelectAllContents"
+               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+               {
+                   paramCollection.AddWithValue("@itemNum", model.ItemNum);
+                   paramCollection.AddWithValue("@pageNum", model.PageNum);
+               }
+               , singleRecordMapper: delegate (IDataReader reader, short set)
+               {
+                   ContentItem singleContent = new ContentItem();
+                   int startingIndex = 0; //starting ordinal
+
+                   singleContent.Id = reader.GetSafeInt32(startingIndex++);
+                   singleContent.ContentTitle = reader.GetSafeString(startingIndex++);
+                   singleContent.UserId = reader.GetSafeInt32(startingIndex++);
+
+                   if (list == null)
+                   {
+                       list = new List<ContentItem>();
+                   }
+
+                   list.Add(singleContent);
+
+               });
+
+            return list;
+        }
+    }
+}
---
+++ ContentItemWidget.js
@@ -0,0 +1,156 @@
+﻿
+(function () {
+    angular.module('BlakWealth')
+        .factory('contentItemFactory', contentItemFactory);
+    contentItemFactory.$inject = ['$http', '$q'];
+
+
+    function contentItemFactory($http, $q) {
+
+        function _get(activeId) {
+            settings = {
+                url: '/api/contentitemcomponent/' + activeId,
+                cache: false,
+                method: "GET",
+                responseType: "json",
+            };
+            return $http(settings)
+                .then(_getSuccess, _getError);
+        }
+
+        function _getSuccess(response) {
+            return response;
+        }
+
+        function _getError(error) {
+            return $q.reject("this is a get error");
+        }
+        return {
+            get: _get
+        };
+    }
+})();
+
+(function () {
+    angular.module('BlakWealth')
+        .controller('contentItemController', contentItemController);
+    contentItemController.$inject = ['contentItemFactory', '$sce', 'alertService', '$window'];
+
+    function contentItemController(contentItemFactory, $sce, alertService, $window) {
+        var vm = this;
+        vm.contentItemFactory = contentItemFactory;
+        vm.data = {};
+        vm.get = _getData;
+        vm.$onChanges = _inIt;
+
+        function _inIt() {
+            if (vm.contentId) {
+                vm.get(vm.contentId);
+            }
+            else {
+                vm.data = {};
+                makeFalse();
+            }
+        }
+
+        function _getData(id) {
+            vm.contentItemFactory.get(id)
+                .then(_getDataSuccess, _getDataError);
+        }
+
+        function makeFalse() {
+            vm.showVideo = false;
+            vm.showImage = false;
+            vm.showLink = false;
+            vm.showAudio = false;
+            vm.showAmazon = false;
+            vm.showText = false;
+            vm.getContentInfo({ info: null });
+        }
+
+        function _getDataSuccess(response) {
+
+            makeFalse();
+
+            if (response.data.item.contentType == "www.youtube.com") {
+                var youtubeUrl = response.data.item.contents;
+                youtubeGetID(youtubeUrl);
+                vm.showVideo = true;
+            } else if (response.data.item.contentType == "vimeo.com") {
+                var vimeoUrl = response.data.item.contents;
+                vimeoID(vimeoUrl);
+                vm.showVideo = true;
+            } else if (response.data.item.contentType == "image") {
+                vm.data.contents = response.data.item.contents;
+                vm.showImage = true;   
+            } else if (response.data.item.contentType == "link") {
+                vm.data.contents = $sce.trustAsResourceUrl(response.data.item.contents);
+                vm.showLink = true;
+            } else if (response.data.item.contentType == "audio") {
+                vm.data.contents = $sce.trustAsResourceUrl(response.data.item.contents);
+                vm.showAudio = true;
+            } else if (response.data.item.contentType == "video") {
+                vm.data.contents = response.data.item.contents;
+                vm.showAmazon = true;
+            } else if (response.data.item.contentType == "markdown") {
+
+                var converter = new showdown.Converter(),
+                    text = response.data.item.contents,
+                    html = converter.makeHtml(text);
+
+                vm.data.contents = $sce.trustAsHtml(html);
+                vm.showText = true;
+            }
+
+            vm.getContentInfo({ info: response.data.item})
+
+        }
+
+        function _getDataError(error) {
+            alertService.error("There was an error in retrieving the data");
+        }
+
+        function youtubeGetID(url) {
+            var ID = '';
+            url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+            if (url[2] !== undefined) {
+                ID = url[2].split(/[^0-9a-z_\-]/i);
+                ID = ID[0];
+            }
+            else {
+                ID = url;
+            }
+            showYoutube(ID);
+        }
+
+        function showYoutube(Id) {
+            vm.data.contents = $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + Id);
+        }
+
+        function vimeoID(url) {
+            var vimeoRegex = /https?:\/\/(?:www\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/;
+            var match = url.match(vimeoRegex);
+            showVimeo(match[3]);
+        }
+
+        function showVimeo(Id) {
+            vm.data.contents = $sce.trustAsResourceUrl("https://player.vimeo.com/video/" + Id);
+        }
+    }
+
+})();
+
+(function () {
+    "use strict";
+
+    var app = angular.module("BlakWealth");
+
+    app.component("contentItem", {
+        templateUrl: "contentitemwidget/contentitemwidget.html",
+        controller: 'contentItemController',
+        bindings: {
+            contentId: '<',
+            getContentInfo: '&'
+        }
+    });
+})();
